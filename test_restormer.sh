e99ace74efc852cf49344d0d4dabae4a38f8ce25
Jiseung <js43o@naver.com>
2026-05-14 16:11:08 +0900

    ✨ HF loss 아키텍처(12.6~) 추가 및 기존 코드와 분리

--- test_restormer.sh
+++ test_restormer.sh
@@ -3,7 +3,7 @@
 # Test WFEN on Helen and CelebA test dataset
 # ================================================================================
 
-python test.py --gpus 1 --model restormer --name 12-4_seperated-iwt_plus_batch8 \
+python test.py --gpus 1 --model new_restormer --name 12-7_seperated-iwt_hf-ssim-loss \
     --load_size 128 --dataset_name validation --dataroot /vcl2/Jiseung/datasets/lfw_custom-aligned_validation \
-    --pretrain_model_path check_points/12-4_seperated-iwt_plus/latest_net_G.pth \
-    --save_as_dir results/12-4_seperated-iwt_plus/lfw_custom-aligned_validation
+    --pretrain_model_path check_points/12-7_seperated-iwt_hf-ssim-loss/latest_net_G.pth \
+    --save_as_dir results/12-7_seperated-iwt_hf-ssim-loss/lfw_custom-aligned_validation
